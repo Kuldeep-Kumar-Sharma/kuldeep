b91477ba2ccae4546810c996aebf7ee6a92cbd82
Kuldeep Kumar <sharman94kuldeep@gmail.com>
2025-07-11 11:22:46 +0200

    css updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgIf } from '@angular/common';
+
+interface ProfileData {
+  name: string;
+  location: string;
+  role: string;
+  summary: string;
+  email: string;
+  phone: string;
+  linkedin: string;
+  github: string;
+  photo: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  providers: [HttpClient],
+  imports: [NgIf],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  profile: ProfileData | null = null;
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<ProfileData>('assets/data/profile-data.json')
+      .subscribe((data) => (this.profile = data));
+  }
 }
